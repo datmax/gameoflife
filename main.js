7868f8868432d6e1a635fc0e694d6087a5ae9bad
Max Datsun <unrusso@live.com>
2018-01-15 20:02:09 +0100

    Updated main.js
    Now it works fine, re-implemented the grid system in an efficient way.

--- main.js
+++ main.js
@@ -5,89 +5,65 @@
   Any type of feedback is appreciated.
 */
 
-let size = 10; //size of a square
+let size = 5; //size of a square
 let board = [];
+let clonedBoard = [];
+let width, height;
 
-var canvas = document.getElementById("c");
-canvas.width = 100;
-canvas.height = 100;
-var ctx = canvas.getContext("2d");
+const canvas = document.getElementById("c");
+width = canvas.width = 600;
+height = canvas.height = 600;
+const ctx = canvas.getContext("2d");
 
 function generate(){
-    for(let i = 0 ; i < canvas.width; i+= size){
-        for(let j = 0; j < canvas.height; j+= size){
-            let alive = Math.random();
-            if(alive >= 0.8){
-                ctx.fillStyle = "black";
-            }
-            else{
-                ctx.fillStyle = "white";
-            }
-            board.push({x: i, y: j, color: ctx.fillStyle})
-            ctx.fillRect(i, j, size, size);
-        }
-    }
-}
-
-function update(){
-    render();
-    nextGeneration();
-}
-
-/*This is to check wheter a cell will be alive or dead in the next gen.
-  Rules are on the wiki: https://en.wikipedia.org/wiki/Conway's_Game_of_Life*/
-function isAlive(x, y){
-    let alive = 0;
-
-    if(ctx.getImageData(x - 1, y, 1, 1).data[0] === 0 && ctx.getImageData(x - 1, y, 1, 1).data[3] !== 0){
-        alive+=1;
-    }
-    if(ctx.getImageData(x + 1, y, 1, 1).data[0] === 0 && ctx.getImageData(x + 1, y , 1, 1).data[3] !== 0){
-        alive+=1;
-    }
-    if(ctx.getImageData(x, y + size, 1, 1).data[0] === 0 && ctx.getImageData(x, y + size, 1, 1).data[3] !== 0 ){
-        alive+=1;
-    }
-    if(ctx.getImageData(x, y - size, 1, 1).data[0] === 0 && ctx.getImageData(x, y - size, 1, 1).data[3] !== 0 ){
-        alive+=1;
-    }
-    if(ctx.getImageData(x + 1, y + size, 1, 1).data[0] === 0 && ctx.getImageData(x + 1, y + size, 1, 1).data[3] !== 0 ){
-        alive+=1;
-    }
-    if(ctx.getImageData(x - 1, y + size, 1, 1).data[0] === 0 && ctx.getImageData(x - 1, y + size, 1, 1).data[3] !== 0 ){
-        alive+=1;
-    }
-    if(ctx.getImageData(x + 1, y - size, 1, 1).data[0] === 0 && ctx.getImageData(x + 1, y - size, 1, 1).data[3] !== 0 ){
-        alive+=1;
-    }
-    if(ctx.getImageData(x - 1, y - size, 1, 1).data[0] === 0 && ctx.getImageData(x - 1, y - size, 1, 1).data[3] !== 0 ){
-        alive += 1;
-    }
-    return alive;
-}
-
-function nextGeneration(){
-    for(let i = 0; i < board.length; i++){
-        if(isAlive(board[i].x, board[i].y) < 2){
-            board[i].color = "white";
-        }
-        if(isAlive(board[i].x, board[i].y) === 3 || isAlive(board[i].x, board[i].y) === 2){
-            board[i].color = "black";
-        }
-        if(isAlive(board[i].x, board[i].y) > 3){
-            board[i].color = "white";
+    for(let x = -size ; x < width + size; x+= size){
+        board[x] = [];
+        clonedBoard[x] = [];
+        for (let y = -size; y < height + size; y += size) {
+            ctx.fillStyle = Math.random() > .5 ? "black" : "white";
+            board[x][y] = ctx.fillStyle === "#000000";
+            ctx.fillRect(x, y, size, size);
         }
     }
 }
 
 
-function render(){
-    for(let i = 0; i < board.length; i++){
-        ctx.fillStyle = board[i].color;
-        ctx.fillRect(board[i].x, board[i].y, size, size);
+//Rules are on the wiki: https://en.wikipedia.org/wiki/Conway's_Game_of_Life 
+function update() {
+    for (let x = 0; x < width; x += size) {
+      for (let y = 0; y < height; y += size) {
+        let isAlive = board[x][y];
+  
+        // Count the alive neighbours
+        const neighbours =
+          board[x-size][y-size] + board[x][y-size] + board[x+size][y-size] + // up row
+          board[x-size][y] + board[x+size][y] + // middle row
+          board[x-size][y+size] + board[x][y+size] + board[x+size][y+size]; // down row
+  
+        // Algorithm
+        if (isAlive) {
+          if (neighbours > 3) { isAlive = 0; } // Any live cell with more than three live neighbours dies, as if by overpopulation.
+          else if (neighbours > 1) { isAlive = 1; } // Any live cell with two or three live neighbours lives on to the next generation.
+          else { isAlive = 0; } // Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+  
+        } else if (neighbours === 3) {
+          isAlive = 1; // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+        }
+  
+        // Render
+        clonedBoard[x][y] = isAlive;
+        ctx.fillStyle = isAlive ? "black" : "white";
+        ctx.fillRect(x, y, size, size);
+      }
     }
-}
+  
+    // Assign the cloned board to the real one
+    for (let x = -size; x < width + size; x += size) {
+      board[x] = clonedBoard[x].slice();
+    }
+  }
+
 
 
 generate();
-setInterval(update,2000);
+setInterval(update,60);
